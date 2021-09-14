1ffc145596d36f9053411b6e40eb05e67ec92ec5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-14 12:40:42 -0500

    fix: use pip3 if available

--- src/utils/setup/setupPip.ts
+++ src/utils/setup/setupPip.ts
@@ -1,10 +1,12 @@
 import { exec } from "@actions/exec"
+import which from "which"
 
 /** A function that installs a package using pip */
 export async function setupPip(name: string, version?: string) {
-  // pip3 install conan --upgrade
+  // check if it exists
+  const pip = which.sync("pip3", { nothrow: true }) !== null ? "pip3" : "pip"
 
-  const exit = await exec("pip", ["install", "--user", version !== undefined ? `${name}==${version}` : name])
+  const exit = await exec(pip, ["install", version !== undefined ? `${name}==${version}` : name])
   if (exit !== 0) {
     throw new Error(`Failed to install ${name} ${version}`)
   }
