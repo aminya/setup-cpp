bf743570e779103e0f859ceb63f3934a35451c98
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-14 15:41:32 -0500

    test: exclude setup-python from the tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
+  testPathIgnorePatterns: ["<rootDir>/src/python/setup-python/"],
   // coverage
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
   coveragePathIgnorePatterns: ["assets", ".css.d.ts"],
