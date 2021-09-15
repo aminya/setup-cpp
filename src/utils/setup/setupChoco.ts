e522160611955d33691f365d6adc65ebc049652f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-15 03:20:11 -0500

    feat: add setup choco

---
+++ src/utils/setup/setupChoco.ts
@@ -0,0 +1,21 @@
+import { exec } from "@actions/exec"
+import which from "which"
+import { setupChocolatey } from "../../chocolatey/chocolatey"
+
+/** A function that installs a package using choco */
+export async function setupChoco(name: string, version?: string) {
+  if (which.sync("choco", { nothrow: true }) === null) {
+    await setupChocolatey()
+  }
+
+  let exit
+  if (version === undefined) {
+    exit = await exec("choco", ["install", name])
+  } else {
+    exit = await exec("choco", ["install", name, `--version=${version}`])
+  }
+
+  if (exit !== 0) {
+    throw new Error(`Failed to install ${name} ${version}`)
+  }
+}
