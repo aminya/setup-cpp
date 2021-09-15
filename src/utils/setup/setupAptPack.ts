9e16dda2d69ff12cab3e7815cb887389ab8ecf08
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-15 03:39:35 -0500

    perf: cache apt-get update

--- src/utils/setup/setupAptPack.ts
+++ src/utils/setup/setupAptPack.ts
@@ -1,9 +1,13 @@
+/* eslint-disable require-atomic-updates */
 import { exec } from "@actions/exec"
+
+let didUpdate: boolean = false
 
 /** A function that installs a package using apt */
 export async function setupAptPack(name: string, version?: string, updateRepositories: boolean = true) {
-  if (updateRepositories) {
+  if (!didUpdate || updateRepositories) {
     await exec("apt-get", ["update"])
+    didUpdate = true
   }
 
   const exit = await exec("apt-get", ["install", version !== undefined ? `${name}=${version}` : name])
