49ca00ee9fc616f703ca3b9797645411603105a0
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-15 01:29:57 -0500

    feat: add self-contained executables

---
+++ building/pack-exe.js
@@ -0,0 +1,38 @@
+const { exec } = require("@actions/exec")
+
+function getPlatformName() {
+  switch (process.platform) {
+    case "win32": {
+      return "windows"
+    }
+    case "darwin": {
+      return "mac"
+    }
+    default: {
+      return process.platform
+    }
+  }
+}
+
+function main() {
+  let exes
+  if (process.platform === "win32") {
+    exes = [".exe"]
+  } else if (process.platform === "darwin") {
+    exes = [".app", ".sh"]
+  } else {
+    exes = ["", ".sh"]
+  }
+
+  return Promise.all(
+    exes.map((exe) =>
+      exec(
+        `./node_modules/.bin/caxa --input ./dist --output ./exe/setup_cpp_${getPlatformName()}${exe} -- "{{caxa}}/node_modules/.bin/node${exe}" "{{caxa}}/main.js"`
+      )
+    )
+  )
+}
+
+main().then((exit) => {
+  process.exit(exit)
+})
