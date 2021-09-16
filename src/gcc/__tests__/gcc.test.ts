24734937d9915fa144db6027d11a157a44ac864b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-16 18:21:11 -0500

    fix: use gcc 10 on windows

--- src/gcc/__tests__/gcc.test.ts
+++ src/gcc/__tests__/gcc.test.ts
@@ -1,14 +1,16 @@
 import { testBin } from "../../utils/tests/test-helpers"
 import { setupGcc } from "../gcc"
+import { getVersion } from "../../default_versions"
 
 jest.setTimeout(200000)
 describe("setup-gcc", () => {
   it("should setup gcc", async () => {
-    const installInfo = await setupGcc("11", "", process.arch)
+    const version = getVersion("gcc", undefined) ?? "11"
+    const installInfo = await setupGcc(version, "", process.arch)
 
     let gpp = "g++"
     if (process.platform !== "win32") {
-      gpp = "g++-11"
+      gpp = `g++-${version}`
     }
     await testBin(gpp, ["--version"], installInfo?.binDir)
   })
