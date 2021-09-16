ecb7cdd1204da867f69570be27dbd87ca2c91700
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-16 11:49:09 -0500

    fix: fix pip installation path for macos and windows

--- src/utils/setup/setupPipPack.ts
+++ src/utils/setup/setupPipPack.ts
@@ -1,11 +1,13 @@
 /* eslint-disable require-atomic-updates */
-import { exec } from "@actions/exec"
+import { exec, getExecOutput } from "@actions/exec"
 import which from "which"
 import { addPath, startGroup, endGroup } from "@actions/core"
 import { setupPython } from "../../python/python"
 import { isBinUptoDate } from "./version"
 
 let pip: string | undefined
+
+let binDir: string | undefined
 
 /** A function that installs a package using pip */
 export async function setupPipPack(name: string, version?: string) {
@@ -26,17 +28,20 @@ export async function setupPipPack(name: string, version?: string) {
     throw new Error(`Failed to install ${name} ${version}`)
   }
 
-  let binDir: string | undefined
-  if (process.platform === "linux") {
-    try {
+  if (binDir === undefined) {
+    if (process.platform === "linux") {
       binDir = "/home/runner/.local/bin/"
+    } else if (process.platform === "darwin") {
+      binDir = "/usr/local/bin/"
+    } else {
+      binDir = (await getExecOutput("python -c 'import sys; print(sys.base_exec_prefix)'")).stdout
+    }
+    try {
       startGroup(`${binDir} to PATH`)
       addPath(binDir)
     } finally {
       endGroup()
     }
-  } else if (process.platform === "darwin") {
-    binDir = "/usr/local/bin/"
   }
 
   return binDir
