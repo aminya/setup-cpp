d82045ec40436ae83d8d68bb4b2f746a39c20480
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-17 14:35:53 -0500

    chore: add script for disabling debug messages in the release

--- .terserrc.js
+++ .terserrc.js
@@ -1,1 +1,18 @@
-module.exports = require("terser-config-atomic")
+const terserConfig = require("terser-config-atomic")
+
+const compress =
+  typeof terserConfig.compress !== "boolean"
+    ? {
+        ...terserConfig.compress,
+        global_defs: {
+          ...terserConfig.compress.global_defs,
+          "process.env.NODE_DEBUG": false,
+          "process.env.RUNNER_DEBUG": "0",
+        },
+      }
+    : terserConfig.compress
+
+module.exports = {
+  ...terserConfig,
+  compress,
+}
---
+++ building/debug_remove.js
@@ -0,0 +1,16 @@
+const { minify } = require("terser")
+const { readFileSync, writeFileSync } = require("fs")
+const { join, dirname } = require("path")
+
+async function main() {
+  console.log("Disabling debug messages inside @actions/core")
+  const file = join(dirname(__dirname), "dist", "setup_cpp.js")
+  const { code, map } = await minify(readFileSync(file, "utf8"), { sourceMap: true, ...require("../.terserrc.js") })
+
+  writeFileSync(file, code)
+  writeFileSync(`${file}.map`, map)
+}
+
+main().catch(function (err) {
+  throw err
+})
