0c29f0b7a3c88bca3f3d96e4b871bb2a6cf61f24
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-11-21 11:14:44 -0600

    fix: enable shell for vcpkg bootstrap

--- src/vcpkg/vcpkg.ts
+++ src/vcpkg/vcpkg.ts
@@ -12,7 +12,7 @@ export function setupVcpkg(_version: string, _setupCppDir: string, _arch: string
   if (!hasVCPKG || which.sync("vcpkg", { nothrow: true }) === null) {
     execa.sync("git", ["clone", "https://github.com/microsoft/vcpkg"], { cwd: untildify("~/") })
     const vcpkgDir = untildify("~/vcpkg")
-    execa.sync("./vcpkg/bootstrap-vcpkg", { cwd: vcpkgDir })
+    execa.sync("./vcpkg/bootstrap-vcpkg", { cwd: vcpkgDir, shell: true })
     addPath(vcpkgDir)
     hasVCPKG = true
     return { binDir: vcpkgDir }
