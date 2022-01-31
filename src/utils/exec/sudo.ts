d7ccf0506bfbea694a5c9387528a8da52ff5d68f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-01-30 18:15:28 -0800

    fix: use stdio inherit for execa.sync brew and apt

--- src/utils/exec/sudo.ts
+++ src/utils/exec/sudo.ts
@@ -3,7 +3,11 @@ import { isRoot } from "../env/sudo"
 
 export function execSudo(file: string, args: string[], cwd?: string) {
   if (isRoot()) {
-    return execa.command(`sudo ${[file, ...args].map((arg) => `'${arg}'`).join(" ")}`, { shell: true, cwd })
+    return execa.command(`sudo ${[file, ...args].map((arg) => `'${arg}'`).join(" ")}`, {
+      shell: true,
+      cwd,
+      stdio: "inherit",
+    })
   } else {
     return execa(file, args)
   }
