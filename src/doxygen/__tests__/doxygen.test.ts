9ce8af143f88f2fa719071a76a08b7b33c6239f5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-02-03 21:38:59 -0800

    fix: download doxygen binary on linux instead of using apt

--- src/doxygen/__tests__/doxygen.test.ts
+++ src/doxygen/__tests__/doxygen.test.ts
@@ -1,11 +1,12 @@
 import { setupDoxygen } from "../doxygen"
 import { testBin } from "../../utils/tests/test-helpers"
 import { InstallationInfo } from "../../utils/setup/setupBin"
+import { getVersion } from "../../default_versions"
 
 jest.setTimeout(300000)
 describe("setup-doxygen", () => {
   it("should setup doxygen", async () => {
-    const installInfo = await setupDoxygen("", "", process.arch)
+    const installInfo = await setupDoxygen(getVersion("doxygen", undefined), "", process.arch)
 
     await testBin("doxygen", ["--version"], (installInfo as InstallationInfo | undefined)?.binDir)
   })
