8bb24cdcdc6ccc4eb18c4ef5e0d381d28d0803a2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-02-13 19:54:04 -0800

    fix: install setuptools for Conan

--- src/conan/conan.ts
+++ src/conan/conan.ts
@@ -1,6 +1,7 @@
 import { setupPipPack } from "../utils/setup/setupPipPack"
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function setupConan(version: string | undefined, _setupDir: string, _arch: string) {
+export async function setupConan(version: string | undefined, _setupDir: string, _arch: string) {
+  await setupPipPack("setuptools", "")
   return setupPipPack("conan", version)
 }
