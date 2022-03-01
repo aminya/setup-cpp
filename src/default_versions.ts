bc71d0580aa43f52e369c0493b8f43e857f32222
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-03-01 14:01:08 -0800

    fix: update cmake, meson, task

--- src/default_versions.ts
+++ src/default_versions.ts
@@ -1,16 +1,16 @@
 const DefaultVersions: Record<string, string> = {
-  llvm: "13.0.0",
+  llvm: "13.0.0", // https://github.com/llvm/llvm-project/releases
   clangtidy: "13.0.0",
   clangformat: "13.0.0",
-  ninja: "1.10.2",
-  cmake: "3.22.1",
-  gcovr: "5.0",
-  conan: "1.45.0",
-  meson: "0.61.1",
+  ninja: "1.10.2", // https://github.com/ninja-build/ninja/releases
+  cmake: "3.22.2", // https://github.com/Kitware/CMake/releases
+  gcovr: "5.0", // https://pypi.org/project/gcovr/
+  conan: "1.45.0", // https://github.com/conan-io/conan/releases
+  meson: "0.61.2", // https://github.com/mesonbuild/meson/releases
   python: "3.8.10",
-  kcov: "v39",
-  task: "3.10.0",
-  doxygen: "1.9.1",
+  kcov: "v39", // https://github.com/SimonKagstrom/kcov/releases
+  task: "3.11.0", // https://github.com/go-task/task/releases
+  doxygen: "1.9.1", // https://packages.ubuntu.com/search?suite=all&arch=any&searchon=names&keywords=doxygen
   gcc: process.platform === "win32" ? "11.2.0.07112021" : "11",
 }
 
