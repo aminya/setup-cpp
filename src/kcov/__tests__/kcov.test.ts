4cfe0b84c3e139d691060fb6e517e9543f009734
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-04-16 00:42:53 -0700

    fix: fix the kcov38 build src

--- src/kcov/__tests__/kcov.test.ts
+++ src/kcov/__tests__/kcov.test.ts
@@ -1,37 +1,46 @@
 import { setupKcov } from "../kcov"
 import { setupTmpDir, cleanupTmpDir, testBin } from "../../utils/tests/test-helpers"
 import { InstallationInfo } from "../../utils/setup/setupBin"
+import which from "which"
 
 jest.setTimeout(300000)
-async function testKcov(version: string, directory: string) {
-  const { binDir } = (await setupKcov(version, directory, "")) as InstallationInfo
-  await testBin("kcov", ["--version"], binDir)
-  return binDir
-}
-
 describe("setup-Kcov", () => {
   if (process.platform !== "linux") {
     it.todo("should setup kcov on non-linux")
     return
   }
 
-  it("should setup Kcov v39", async () => {
-    const directory = await setupTmpDir("kcov-v39")
-    await testKcov("v39", directory)
-    await cleanupTmpDir("kcov-v39")
+  let directory: string
+  beforeAll(async () => {
+    directory = await setupTmpDir("kcov-v39")
   })
 
-  // TODO
-  // it("should setup Kcov v38", async () => {
-  //   const directory = await setupTmpDir("kcov-v38")
-  //   await testKcov("v38", directory)
+  it("should setup Kcov v39", async () => {
+    const { binDir } = (await setupKcov("39", directory, "")) as InstallationInfo
+    await testBin("kcov", ["--version"], binDir)
+  })
+
+  // it("should find Kcov in the cache", async () => {
+  //   const binDir = await testKcov("v39", directory)
+  //   if (isGitHubCI()) {
+  //     expect(binDir).toMatch(process.env.RUNNER_TOOL_CACHE ?? "hostedtoolcache")
+  //   }
   //   await cleanupTmpDir("kcov-v39")
   // })
 
-  // it("should find Kcov in the cache", async () => {
-  //   const directory = await setupTmpDir("kcov-v39")
-  //   const binDir = await testKcov("v39", directory)
-  //   expect(binDir.includes("hostedtoolcache")).toBeTruthy()
-  //   await cleanupTmpDir("kcov-v39")
-  // })
+  it("should setup Kcov v38", async () => {
+    const directory2 = await setupTmpDir("kcov-v38")
+
+    await setupKcov("38", directory2, "")
+
+    expect(which.sync("kcov", { nothrow: true })).toBeTruthy()
+
+    await testBin("kcov", ["--version"], "usr/local/bin") // because of cmake --install
+
+    await cleanupTmpDir("kcov-v38")
+  })
+
+  afterAll(async () => {
+    await cleanupTmpDir("kcov-v38")
+  })
 })
--- src/utils/setup/extract.ts
+++ src/utils/setup/extract.ts
@@ -2,6 +2,7 @@ import execa from "execa"
 import { mkdirP } from "@actions/io"
 import which from "which"
 import { setupSevenZip } from "../../sevenzip/sevenzip"
+import { warning } from "../io/io"
 export { extractTar, extractXar, extract7z, extractZip } from "@actions/tool-cache"
 
 let sevenZip: string | undefined
@@ -26,6 +27,17 @@ export async function extractTarByExe(file: string, dest: string, flags = ["--st
   } catch {
     // ignore
   }
-  await execa("tar", ["xf", file, "-C", dest, ...flags], { stdio: "inherit" })
+
+  // TODO windows fails to create symlinks
+  // https://github.com/heroku/heroku-slugs/issues/3
+
+  try {
+    await execa("tar", ["xf", file, "-C", dest, ...flags], { stdio: "inherit" })
+  } catch (e) {
+    if (process.platform === "win32" && (e as Error).message.includes("Can't create '\\\\?\\C:")) {
+      warning(`Failed to extract symlink ${file} to ${dest}. Ignoring this symlink.`)
+    }
+  }
+
   return dest
 }
