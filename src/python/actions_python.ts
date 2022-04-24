660f362f49b65ef2bd204fb8c731d5e960213e01
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-04-24 16:39:43 -0700

    fix: do not use setFailed

--- src/python/actions_python.ts
+++ src/python/actions_python.ts
@@ -1,8 +1,8 @@
-import * as finder from "setup-python/src/find-python"
-import * as finderPyPy from "setup-python/src/find-pypy"
+import { useCpythonVersion } from "setup-python/src/find-python"
+import { findPyPyVersion } from "setup-python/src/find-pypy"
 import { existsSync } from "fs"
 import { info, warning } from "../utils/io/io"
-import * as core from "@actions/core"
+import { debug } from "@actions/core"
 import path from "path"
 import { isGitHubCI } from "../utils/env/isci"
 
@@ -12,33 +12,27 @@ function isPyPyVersion(versionSpec: string) {
 
 export async function setupActionsPython(version: string, _setupDir: string, arch: string) {
   if (process.env.AGENT_TOOLSDIRECTORY?.trim()) {
-    core.debug(`Python is expected to be installed into AGENT_TOOLSDIRECTORY=${process.env.AGENT_TOOLSDIRECTORY}`)
+    debug(`Python is expected to be installed into AGENT_TOOLSDIRECTORY=${process.env.AGENT_TOOLSDIRECTORY}`)
     process.env.RUNNER_TOOL_CACHE = process.env.AGENT_TOOLSDIRECTORY
   } else {
-    core.debug(`Python is expected to be installed into RUNNER_TOOL_CACHE==${process.env.RUNNER_TOOL_CACHE}`)
+    debug(`Python is expected to be installed into RUNNER_TOOL_CACHE==${process.env.RUNNER_TOOL_CACHE}`)
   }
-  try {
-    if (version) {
-      let pythonVersion: string
-      if (isPyPyVersion(version)) {
-        const installed = await finderPyPy.findPyPyVersion(version, arch)
-        pythonVersion = `${installed.resolvedPyPyVersion}-${installed.resolvedPythonVersion}`
-        info(
-          `Successfully setup PyPy ${installed.resolvedPyPyVersion} with Python (${installed.resolvedPythonVersion})`
-        )
-      } else {
-        const installed = await finder.useCpythonVersion(version, arch)
-        pythonVersion = installed.version
-        info(`Successfully setup ${installed.impl} (${pythonVersion})`)
-      }
-
-      const cache = "pip" // core.getInput("cache") // package manager used for caching
-
-      const { cacheDependencies } = await import("./cache")
-      await cacheDependencies(cache, pythonVersion)
+  if (version) {
+    let pythonVersion: string
+    if (isPyPyVersion(version)) {
+      const installed = await findPyPyVersion(version, arch)
+      pythonVersion = `${installed.resolvedPyPyVersion}-${installed.resolvedPythonVersion}`
+      info(`Successfully setup PyPy ${installed.resolvedPyPyVersion} with Python (${installed.resolvedPythonVersion})`)
+    } else {
+      const installed = await useCpythonVersion(version, arch)
+      pythonVersion = installed.version
+      info(`Successfully setup ${installed.impl} (${pythonVersion})`)
     }
-  } catch (err) {
-    core.setFailed((err as Error).message)
+
+    const cache = "pip" // core.getInput("cache") // package manager used for caching
+
+    const { cacheDependencies } = await import("./cache")
+    await cacheDependencies(cache, pythonVersion)
   }
 
   if (isGitHubCI()) {
