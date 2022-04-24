861fbd37a173ffef851b11c4e4233e67580b98ec
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-04-24 16:24:58 -0700

    fix: use /Users/ instead of /home/ on MacOS

--- src/utils/path/untildify.ts
+++ src/utils/path/untildify.ts
@@ -5,7 +5,11 @@ import { isRoot } from "../env/sudo"
 export function untildify_user(path: string) {
   if (isRoot() && typeof process.env.SUDO_USER === "string") {
     // use the user profile even if root
-    return join("/home/", process.env.SUDO_USER, path)
+    if (process.platform === "darwin") {
+      return join("/Users/", process.env.SUDO_USER, path)
+    } else {
+      return join("/home/", process.env.SUDO_USER, path)
+    }
   } else {
     return untildify(`~/${path}`)
   }
