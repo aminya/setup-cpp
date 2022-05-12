4109838bca9f254ce264f2d89c3e9bcdc7b09da2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-05-11 17:13:27 -0700

    fix: use escape-path-with-spaces to fix new windows paths

--- src/utils/path/escape_space.ts
+++ src/utils/path/escape_space.ts
@@ -1,11 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import escape from "escape-path-with-spaces"
+
 /// Escape the spaces in the given path
 export function escapeSpace(path: string | undefined): string {
   if (path === undefined) {
     return ""
   }
-  if (process.platform === "win32") {
-    return path.replace(/(\s+)/g, "%20")
-  } else {
-    return path.replace(/(\s+)/g, "\\$1")
-  }
+  return escape(path)
 }
