ba381879e0ddc31f3834c4dc778287a3112a313f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-06-25 16:51:28 -0700

    fix: do not set a default version for python
    Because the version handling for the backends is totally incompatible

--- src/default_versions.ts
+++ src/default_versions.ts
@@ -12,7 +12,6 @@ const DefaultVersions: Record<string, string> = {
   gcovr: "5.1", // https://pypi.org/project/gcovr/
   conan: "1.48.2", // https://github.com/conan-io/conan/releases
   meson: "0.62.2", // https://github.com/mesonbuild/meson/releases
-  python: "3.8.10", // semver
   kcov: "40", // https://github.com/SimonKagstrom/kcov/releases
   task: "3.12.1", // https://github.com/go-task/task/releases
   doxygen: "1.9.4", // https://www.doxygen.nl/download.html // https://packages.ubuntu.com/search?suite=all&arch=any&searchon=names&keywords=doxygen // https://formulae.brew.sh/formula/doxygen
