5692a92fdb39c46236c6e7dbc136238c9dc1d42b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-06-25 17:16:32 -0700

    fix: update setup-python

--- src/python/actions_python.ts
+++ src/python/actions_python.ts
@@ -8,26 +8,48 @@ import { isGitHubCI } from "../utils/env/isci"
 import { cacheDependencies } from "./actions_cache"
 
 function isPyPyVersion(versionSpec: string) {
-  return versionSpec.startsWith("pypy-")
+  return versionSpec.startsWith("pypy")
 }
+
+// function resolveVersionInput(version: string): string {
+//   let versionFile = getInput("python-version-file")
+
+//   if (version && versionFile) {
+//     warning("Both python-version and python-version-file inputs are specified, only python-version will be used")
+//   }
+
+//   if (version) {
+//     return version
+//   }
+
+//   versionFile = versionFile || ".python-version"
+//   if (!existsSync(versionFile)) {
+//     throw new Error(`The specified python version file at: ${versionFile} does not exist`)
+//   }
+//   version = readFileSync(versionFile, "utf8")
+//   info(`Resolved ${versionFile} as ${version}`)
+
+//   return version
+// }
 
 export async function setupActionsPython(version: string, _setupDir: string, arch: string) {
   if (process.env.AGENT_TOOLSDIRECTORY?.trim()) {
-    debug(`Python is expected to be installed into AGENT_TOOLSDIRECTORY=${process.env.AGENT_TOOLSDIRECTORY}`)
-    process.env.RUNNER_TOOL_CACHE = process.env.AGENT_TOOLSDIRECTORY
+    debug(`Python is expected to be installed into AGENT_TOOLSDIRECTORY=${process.env["AGENT_TOOLSDIRECTORY"]}`)
+    process.env["RUNNER_TOOL_CACHE"] = process.env["AGENT_TOOLSDIRECTORY"]
   } else {
-    debug(`Python is expected to be installed into RUNNER_TOOL_CACHE==${process.env.RUNNER_TOOL_CACHE}`)
+    debug(`Python is expected to be installed into RUNNER_TOOL_CACHE==${process.env["RUNNER_TOOL_CACHE"]}`)
   }
+  // const version = resolveVersionInput(versionGiven)
   if (version) {
     let pythonVersion: string
     if (isPyPyVersion(version)) {
       const installed = await findPyPyVersion(version, arch)
       pythonVersion = `${installed.resolvedPyPyVersion}-${installed.resolvedPythonVersion}`
-      info(`Successfully setup PyPy ${installed.resolvedPyPyVersion} with Python (${installed.resolvedPythonVersion})`)
+      info(`Successfully set up PyPy ${installed.resolvedPyPyVersion} with Python (${installed.resolvedPythonVersion})`)
     } else {
       const installed = await useCpythonVersion(version, arch)
       pythonVersion = installed.version
-      info(`Successfully setup ${installed.impl} (${pythonVersion})`)
+      info(`Successfully set up ${installed.impl} (${pythonVersion})`)
     }
 
     const cache = "pip" // core.getInput("cache") // package manager used for caching
