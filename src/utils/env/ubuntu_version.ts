0fd405d4e13201859d9b2d0ed80741d6d3b4aa7c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-07-27 00:49:07 -0700

    fix: install lsb_release if it is missing on ubuntu

--- src/utils/env/ubuntu_version.ts
+++ src/utils/env/ubuntu_version.ts
@@ -1,8 +1,13 @@
 import { getUbuntuVersion } from "ubuntu-version"
+import which from "which"
+import { setupAptPack } from "../setup/setupAptPack"
 import { isUbuntu } from "./isUbuntu"
 
 export async function ubuntuVersion(): Promise<number[] | null> {
   if (isUbuntu()) {
+    if (which.sync("lsb_release", { nothrow: true }) === null) {
+      setupAptPack("lsb-release")
+    }
     const versionSplitted = await getUbuntuVersion()
 
     if (versionSplitted.length === 0) {
