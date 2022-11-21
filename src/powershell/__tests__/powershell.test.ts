488d899a8fac02806bbf82abd9e91819662a9ab4
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-11-20 18:44:41 -0800

    test: skip powershell installation in GitHub Actions

--- src/powershell/__tests__/powershell.test.ts
+++ src/powershell/__tests__/powershell.test.ts
@@ -1,10 +1,16 @@
 import { setupPowershell } from "../powershell"
 import { testBin } from "../../utils/tests/test-helpers"
 import { getVersion } from "../../versions/versions"
+import ciDetect from "@npmcli/ci-detect"
 
 jest.setTimeout(300000)
 describe("setup-powershell", () => {
   it("should setup powershell", async () => {
+    if (process.platform === "win32" && ciDetect() == "github-actions") {
+      // results in errors
+      return
+    }
+
     const installInfo = await setupPowershell(getVersion("powershell", undefined), "", process.arch)
 
     await testBin("pwsh", ["--version"], installInfo.binDir)
