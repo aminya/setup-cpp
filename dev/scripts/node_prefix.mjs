f36323172f220f4f28fc34f68572eeb7dee81e55
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-04-22 03:03:35 -0700

    fix: remove node: prefix from the build

---
+++ dev/scripts/node_prefix.mjs
@@ -0,0 +1,19 @@
+import { readFile, writeFile } from "fs/promises"
+import glob from "fast-glob"
+
+const distFolder = "dist"
+
+// remove node: prefix from the scripts in the dist folder
+
+async function main() {
+  const files = await glob([`${distFolder}/**/*.js`, `${distFolder}/**/*.js.map`], { absolute: true, onlyFiles: true })
+  await Promise.all(
+    files.map(async (file) => {
+      const content = await readFile(file, "utf8")
+      const newContent = content.replace(/require\("node:/g, 'require("')
+      await writeFile(file, newContent)
+    })
+  )
+}
+
+await main()
