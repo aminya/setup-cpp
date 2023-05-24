f5342df15b2aa954c3469ad9decd398fa1da2916
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-05-24 11:18:46 -0700

    feat: check if update-notifier is installed before using it

--- src/check-updates.ts
+++ src/check-updates.ts
@@ -2,21 +2,26 @@ import { warning } from "ci-log"
 import { promises } from "fs"
 const { readFile } = promises
 import { join } from "path"
+import { isInstalled } from "./utils/std/resolve"
 
 // auto self update notifier
 export async function checkUpdates() {
   try {
-    const [un, packageJsonString] = await Promise.all([
-      import("update-notifier"),
-      readFile(join(__dirname, "..", "package.json"), "utf8"),
-    ])
+    if (await isInstalled("update-notifier")) {
+      const [un, packageJsonString] = await Promise.all([
+        import("update-notifier"),
+        readFile(join(__dirname, "..", "package.json"), "utf8"),
+      ])
 
-    const packageJson = JSON.parse(packageJsonString)
+      const packageJson = JSON.parse(packageJsonString)
 
-    // the types do not match the actual export
-    const updateNotifier = un as unknown as (typeof un)["default"]
+      // the types do not match the actual export
+      const updateNotifier = un as unknown as (typeof un)["default"]
 
-    updateNotifier({ pkg: packageJson }).notify()
+      updateNotifier({ pkg: packageJson }).notify()
+    } else {
+      warning("setup-cpp could not find its dependency update-notifier, skipping check for updates")
+    }
   } catch (err) {
     warning(`Failed to check for updates: ${err instanceof Error ? err.message + err.stack : err}`)
   }
