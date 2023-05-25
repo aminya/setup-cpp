5c3b317006f66d3e28ae5373fc60e4883ab6fdbc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-05-24 22:07:48 -0700

    fix: use simple-update-notifier instead of update-notifier

--- src/check-updates.ts
+++ src/check-updates.ts
@@ -1,27 +1,11 @@
 import { warning } from "ci-log"
-import { promises } from "fs"
-const { readFile } = promises
-import { join } from "path"
-import { isInstalled } from "./utils/std/resolve"
+import updateNotifier from "simple-update-notifier"
+import packageJson from "../package.json" assert { type: "json" }
 
 // auto self update notifier
 export async function checkUpdates() {
   try {
-    if (await isInstalled("update-notifier")) {
-      const [un, packageJsonString] = await Promise.all([
-        import("update-notifier"),
-        readFile(join(__dirname, "..", "package.json"), "utf8"),
-      ])
-
-      const packageJson = JSON.parse(packageJsonString)
-
-      // the types do not match the actual export
-      const updateNotifier = un as unknown as (typeof un)["default"]
-
-      updateNotifier({ pkg: packageJson }).notify()
-    } else {
-      warning("setup-cpp could not find its dependency update-notifier, skipping check for updates")
-    }
+    await updateNotifier({ pkg: packageJson })
   } catch (err) {
     warning(`Failed to check for updates: ${err instanceof Error ? err.message + err.stack : err}`)
   }
