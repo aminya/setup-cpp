8d969d8dbd276f648ecaa7c69c754081b30aa419
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-09-05 11:04:10 -0700

    test: remove nala after testing it

--- src/nala/__tests__/nala.test.ts
+++ src/nala/__tests__/nala.test.ts
@@ -1,6 +1,7 @@
 import { setupNala } from "../nala"
 import { testBin } from "../../utils/tests/test-helpers"
 import { isUbuntu } from "../../utils/env/isUbuntu"
+import { execRootSync } from "admina"
 
 jest.setTimeout(300000)
 describe("setup-nala", () => {
@@ -11,5 +12,11 @@ describe("setup-nala", () => {
   it("should setup nala", async () => {
     const installInfo = await setupNala("", "", process.arch)
     await testBin("nala", ["--version"], installInfo?.binDir)
+  })
+
+  afterAll(() => {
+    // remove nala to run the rest of the tests with apt-get
+    execRootSync("apt-get", ["remove", "-y", "nala"])
+    execRootSync("apt-get", ["remove", "-y", "nala-legacy"])
   })
 })
