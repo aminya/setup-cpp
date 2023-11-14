0f59570d63f5ed9ea1929639963c91ba7585ef03
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-11-14 11:10:02 -0800

    fix: update Conan, Meson, Task

--- src/versions/default_versions.ts
+++ src/versions/default_versions.ts
@@ -24,10 +24,10 @@ export const DefaultVersions: Record<string, string | undefined> = {
   ninja: "1.11.1", // https://github.com/ninja-build/ninja/releases
   cmake: "3.27.4", // https://github.com/Kitware/CMake/releases
   gcovr: "6.0", // https://pypi.org/project/gcovr/
-  conan: "1.60.2", // https://github.com/conan-io/conan/releases
-  meson: "1.2.1", // https://github.com/mesonbuild/meson/releases
+  conan: "1.62.0", // https://github.com/conan-io/conan/releases
+  meson: "1.2.3", // https://github.com/mesonbuild/meson/releases
   kcov: "42", // https://github.com/SimonKagstrom/kcov/releases
-  task: "3.29.1", // https://github.com/go-task/task/releases
+  task: "3.31.0", // https://github.com/go-task/task/releases
   doxygen: isArch() ? "1.9.8-1" : "1.9.8", // https://www.doxygen.nl/download.html // https://packages.ubuntu.com/search?suite=all&arch=any&searchon=names&keywords=doxygen // https://formulae.brew.sh/formula/doxygen // https://archlinux.org/packages/extra/x86_64/doxygen/
   gcc: isArch() ? "13.2.1-3" : "13", // https://github.com/brechtsanders/winlibs_mingw/releases and // https://packages.ubuntu.com/search?suite=all&arch=any&searchon=names&keywords=gcc
   //mingw: isArch() ? "12.2.0-1" : "8", // https://packages.ubuntu.com/search?suite=all&arch=any&searchon=names&keywords=mingw-w64 // https://archlinux.org/packages/extra/x86_64/mingw-w64-gcc/
