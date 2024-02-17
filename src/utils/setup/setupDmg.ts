32bae2b687380e42c5347550265e2d166f68225b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-02-17 01:30:22 -0800

    fix: use dynamic import for ArchiveHdi

--- src/utils/setup/setupDmg.ts
+++ src/utils/setup/setupDmg.ts
@@ -1,7 +1,8 @@
-import { ArchiveHdi } from "@shockpkg/archive-files/esm/archive/hdi.mjs"
 import { join } from "path"
 
 export async function setupDmg(path: string, destDir: string) {
+  const { ArchiveHdi } = await import("@shockpkg/archive-files/esm/archive/hdi.mjs")
+
   const dmg = new ArchiveHdi(path)
   await dmg.read(async (entry) => {
     await entry.extract(join(destDir, entry.path))
