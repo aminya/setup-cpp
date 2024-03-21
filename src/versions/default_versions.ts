29399c3aa7917476266bd2114e627537780747f2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-03-21 11:37:25 -0700

    fix: fix linux binaries for llvm

--- src/versions/default_versions.ts
+++ src/versions/default_versions.ts
@@ -6,14 +6,15 @@ import { isArch } from "../utils/env/isArch"
 function getLLVMDefault() {
   switch (process.platform) {
     case "win32":
-      return "18.1.1"
+      return "18.1.2"
     case "linux":
       // used for non-ubuntu (Fedora, Arch)
-      return "18.1.1-ubuntu-22.04"
+      // the suffixes relate to the suffix in the llvm releases
+      return "17.0.6-ubuntu-22.04"
     case "darwin":
       return "15.0.3"
     default:
-      return "18.1.1"
+      return "18.1.2"
   }
 }
 
@@ -53,24 +54,25 @@ export const DefaultLinuxVersion: Record<string, Record<number, string> | undefi
     22: "8.0.0-1",
     20: "7.0.0-2",
   },
+  // the suffixes relate to the suffix in the llvm releases
   llvm: {
-    22: "18.1.1-ubuntu-22.04",
-    20: "18.1.1-ubuntu-22.04",
-    18: "18.1.1-ubuntu-22.04",
+    22: "18.1.2",
+    20: "18.1.2",
+    18: "15.0.6-ubuntu-18.04",
     16: "15.0.6-ubuntu-18.04",
     14: "13.0.0-ubuntu-16.04",
   },
   clangtidy: {
-    22: "18.1.1-ubuntu-22.04",
-    20: "18.1.1-ubuntu-22.04",
-    18: "18.1.1-ubuntu-22.04",
+    22: "18.1.2",
+    20: "18.1.2",
+    18: "15.0.6-ubuntu-18.04",
     16: "15.0.6-ubuntu-18.04",
     14: "13.0.0-ubuntu-16.04",
   },
   clangformat: {
-    22: "18.1.1-ubuntu-22.04",
-    20: "18.1.1-ubuntu-22.04",
-    18: "18.1.1-ubuntu-22.04",
+    22: "18.1.2",
+    20: "18.1.2",
+    18: "15.0.6-ubuntu-18.04",
     16: "15.0.6-ubuntu-18.04",
     14: "13.0.0-ubuntu-16.04",
   },
