0660ca7619ec761c1dd9f3e4ac7e730e6efbdfc5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-08-23 15:33:10 -0700

    fix: add module type to packages + fix running the packages tests

--- packages/envosman/src/index.ts
+++ packages/envosman/src/index.ts
@@ -1,3 +1,3 @@
-export { addEnv } from "./add-env.js"
-export { addPath } from "./add-path.js"
+export { addEnv, AddEnvOptions } from "./add-env.js"
+export { addPath, AddPathOptions } from "./add-path.js"
 export { finalizeRC, sourceRCInRc as sourceRC } from "./rc-file.js"
